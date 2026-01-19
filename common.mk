44a1b1ffcc57d484613a04964e3cd3159e5f0c05
SuperAviation001 <superaviation001@gmail.com>
2026-01-19 23:03:07 +1100

    a13-common: Update proprietary files list

--- common.mk
+++ common.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl:32 \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio@7.0-impl:32 \
-    android.hardware.audio.service \
+    android.hardware.audio.service:32 \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
