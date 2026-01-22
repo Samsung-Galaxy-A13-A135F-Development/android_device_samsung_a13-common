fdd347a5622650fded023a11bf9355d28afae6b2
SuperAviation001 <superaviation001@gmail.com>
2026-01-22 23:33:58 +1100

    Revert "a13-common: Set DPI to 360"
    This reverts commit 2a50b3f523d893961e5b888074150c104bccac6f.
    * Didn't really look better

--- common.mk
+++ common.mk
@@ -122,8 +122,8 @@ PRODUCT_PACKAGES += \
 # Graphics
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 360dpi
-PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := 400dpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Health
 PRODUCT_PACKAGES += \
