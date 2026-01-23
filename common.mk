19578f594a17cc11bcbbc57711a1392d859891c2
SuperAviation001 <superaviation001@gmail.com>
2026-01-23 17:22:42 +1100

    a13-common: Build libprocessgroup_shim to satisfy libutils-v31

--- common.mk
+++ common.mk
@@ -223,6 +223,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Shims
+PRODUCT_PACKAGES += \
+    libprocessgroup_shim
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
