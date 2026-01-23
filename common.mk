894486fa1832953393e4a53e2770d66b111fc3a1
Dennis Murimi <AndroidHQ254@users.noreply.github.com>
2026-01-23 13:34:29 +1100

    a13-common: Link older libutils for samsung camera provider
    Fixes:
    F DEBUG   : Abort message: 'Error while registering provider service: -2147483648'

--- common.mk
+++ common.mk
@@ -71,10 +71,9 @@ PRODUCT_COPY_FILES += \
     hardware/samsung_slsi/libbt/conf/bt_did.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_did.conf \
     hardware/samsung_slsi/libbt/conf/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
 
-# Camera
-#PRODUCT_PACKAGES += \
-#    android.hardware.camera.provider-service_32.samsung \
-#    libGrallocMapperCamera:32
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v31/arm/arch-arm-armv7-a-neon/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v31.so
 
 # Charger
 PRODUCT_PACKAGES += \
