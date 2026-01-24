33e114647448e5b23eca79678c9c0549f4cd8eb5
SuperAviation001 <superaviation001@gmail.com>
2026-01-24 20:33:52 +1100

    a13-common: remove sepolicy/private

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,7 +178,6 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 ## USB
 $(call soong_config_set,samsungUsbGadgetVars,gadget_name,13600000.dwc3)
