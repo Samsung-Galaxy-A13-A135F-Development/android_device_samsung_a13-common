777d2f4fe337c3a6a0dbef8d9491e9daedcd48a0
SuperAviation001 <superaviation001@gmail.com>
2026-01-26 13:59:11 +1100

    a13-common: Drop HIDL camera stuff

--- common.mk
+++ common.mk
@@ -229,15 +229,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    cameraserver:32 \
-    android.hardware.camera.provider@2.4-legacy \
-    android.hardware.camera.provider@2.5-legacy \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
-    camera.device@3.4-impl \
-    camera.device@3.5-impl \
-    android.hardware.camera.device@3.6.vendor \
     android.hardware.camera.provider-service_32.samsung \
     libGrallocMapperCamera:32
 
