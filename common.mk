2a804023b51ea46b070eb4a2097eab5e9543de9b
SuperAviation001 <superaviation001@gmail.com>
2026-01-27 22:25:13 +1100

    a13-common: Use 32bit graphics stack

--- common.mk
+++ common.mk
@@ -85,14 +85,14 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.composer.hwc3-service.slsi \
-    android.hardware.graphics.allocator@2.0-impl:64 \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
-    gralloc.universal3830
+    android.hardware.composer.hwc3-service.slsi:32 \
+    android.hardware.graphics.allocator@2.0-impl:32 \
+    android.hardware.graphics.allocator@2.0-service:32 \
+    android.hardware.graphics.mapper@2.0-impl-2.1:32 \
+    gralloc.universal3830:32
 
 PRODUCT_PACKAGES += \
-    libGrallocWrapper
+    libGrallocWrapper:32
 
 PRODUCT_PACKAGES += \
     libexynosdisplay:32 \
