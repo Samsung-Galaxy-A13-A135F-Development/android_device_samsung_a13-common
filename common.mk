69e33b5e587bbfa933381cc4711665ca8afc365e
SuperAviation001 <superaviation001@gmail.com>
2026-03-14 22:49:52 +1100

    a13-common: drop camera id 52
    getCameraCharacteristics:1338: Unable to retrieve camera characteristics for unknown device 52: No such file or directory (-2) (code 3)

--- common.mk
+++ common.mk
@@ -238,7 +238,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 $(call soong_config_set_bool,samsungCameraVars,needs_sec_reserved_field,true)
-$(call soong_config_set,samsungCameraVars,extra_ids,23,50,52,54)
+$(call soong_config_set,samsungCameraVars,extra_ids,23,50,54)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service_32.samsung \
